7862a98acdc256f6412a6922c08db525ac948398
Nick Saunders <nick@saunde.rs>
2024-01-13 05:38:23 -0700

    Bump css hooks to v1.7

--- src/css.ts
+++ src/css.ts
@@ -1,34 +1,38 @@
-import type { CSSProperties } from "@builder.io/qwik";
-import { stringifyValue } from "@css-hooks/qwik";
+import { createHooks, stringifyValue } from "@css-hooks/qwik";
 import { recommended } from "@css-hooks/recommended";
-import { buildHooksSystem } from "./css-hooks-core";
 
-const createHooks = buildHooksSystem<CSSProperties>(stringifyValue);
-
-export const [hooks, css] = createHooks({
-  ...recommended({
-    breakpoints: ["640px"],
-    pseudoClasses: [":active", ":focus-visible", ":hover"],
-  }),
-  "&.selected": "&.selected",
-  ".group:hover &": ".group:hover &",
-  "@media (prefers-color-scheme: dark)": {
-    or: [
-      "[data-theme='dark'] &",
-      {
-        and: ["[data-theme='auto'] &", "@media (prefers-color-scheme: dark)"],
-      },
-    ],
+export const [hooks, css] = createHooks(
+  {
+    ...recommended({
+      breakpoints: ["640px"],
+      pseudoClasses: [":active", ":focus-visible", ":hover"],
+    }),
+    "&.selected": "&.selected",
+    ".group:hover &": ".group:hover &",
+    "@media (prefers-color-scheme: dark)": {
+      or: [
+        "[data-theme='dark'] &",
+        {
+          and: ["[data-theme='auto'] &", "@media (prefers-color-scheme: dark)"],
+        },
+      ],
+    },
+    "@media (prefers-color-scheme: light)": {
+      or: [
+        "[data-theme='light'] &",
+        {
+          and: [
+            "[data-theme='auto'] &",
+            "@media (prefers-color-scheme: light)",
+          ],
+        },
+      ],
+    },
   },
-  "@media (prefers-color-scheme: light)": {
-    or: [
-      "[data-theme='light'] &",
-      {
-        and: ["[data-theme='auto'] &", "@media (prefers-color-scheme: light)"],
-      },
-    ],
-  },
-});
+  {
+    sort: true,
+  }
+);
 
 export function renderToString(obj: Parameters<typeof css>[0]) {
   return Object.entries(css(obj))
